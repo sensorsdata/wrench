a4ab4bba2bb9304d9c5ac85d6792348b838c9b2b
wuxue <wuxue@sensorsdata.cn>
2022-03-14 19:50:55 +0800

    UT Case: indexOf

--- test/indexOf.test.js
+++ test/indexOf.test.js
@@ -3,21 +3,23 @@ import indexOf from '../src/indexOf';
 import sinon from 'sinon';
 
 test('test indexOf function', (t) => {
+  var arr = [1, 2, 3];
   var spy = sinon.spy(Array.prototype, 'indexOf');
-  let val = indexOf([1, 2, 3], 1);
+  let val = indexOf(arr, 1);
   t.ok(spy.calledOnce, 'when indexOf called, then Array.prototype.indexOf will be called once.');
   t.equal(val, 0, 'when call indexOf([1, 2, 3], 1), then it returns 0');
 
   sinon.restore();
 
-  sinon.stub(Array.prototype).indexOf = undefined;
-  val = indexOf([1, 2, 3], 1);
-  t.equal(val, 0, 'Array.prototype.indexOf == undefined, when call indexOf([1, 2, 3], 1), then it returns 0');
+  var stub = sinon.stub(Array.prototype, 'indexOf').value(undefined);
 
-  val = indexOf([1, 2, 3], 0);
-  t.equal(val, -1, 'Array.prototype.indexOf == undefined, when call indexOf([1, 2, 3], 0), then it returns -1');
-  
-  sinon.restore();
+  val = indexOf(arr, 1);
+  t.equal(val, 0, 'call indexOf([1, 2, 3], 1) when Array.prototype.indexOf == undefined, then it returns 0');
+
+  val = indexOf(arr, 0);
+  t.equal(val, -1, 'call indexOf([1, 2, 3], 0) when Array.prototype.indexOf == undefined, then it returns -1');
+
+  stub.restore();
 
   t.end();
 });
