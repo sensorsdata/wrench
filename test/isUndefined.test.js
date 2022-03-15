49e80b93856224ddf79c5a4427efa5892346c8d8
wuxue <wuxue@sensorsdata.cn>
2022-03-15 11:48:25 +0800

    UT Case

---
+++ test/isUndefined.test.js
@@ -0,0 +1,16 @@
+import test from 'tape';
+import isUndefined from '../src/isUndefined';
+
+const testCases = [
+  { input: undefined, expect: true },
+  { input: null, expect: false },
+];
+
+test('test isString function', (t) => {
+  testCases.forEach((testCase) => {
+    const val = isUndefined(testCase.input);
+    t.equal(val, testCase.expect, `when call isUndefined("${testCase.input}"), then it returns "${testCase.expect}"`);
+  });
+  
+  t.end();
+});
