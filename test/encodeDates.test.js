9251f202f82c415614ab5c30ad9a78a66c0ccf42
wuxue <wuxue@sensorsdata.cn>
2022-03-16 17:49:58 +0800

    UT Case

---
+++ test/encodeDates.test.js
@@ -0,0 +1,40 @@
+import test from 'tape';
+import encodeDates from '../src/encodeDates';
+
+const testCases = [
+  {
+    input: { a: new Date('2020-02-02 8:0:12') },
+    expect: { a: '2020-02-02 08:00:12.00' },
+  },
+  {
+    input: {
+      a: new Date('2020-02-02 8:0:12'),
+      b: 'test',
+      c: {
+        c_child: new Date('2020-02-03 8:0:12'),
+      },
+    },
+    expect: {
+      a: '2020-02-02 08:00:12.00',
+      b: 'test',
+      c: {
+        c_child: '2020-02-03 08:00:12.00',
+      },
+    },
+  },
+];
+
+test('test map encodeDates', (t) => {
+  testCases.forEach((testCase) => {
+    const val = encodeDates(testCase.input);
+    t.deepEqual(
+      val,
+      testCase.expect,
+      `when call encodeDates(${JSON.stringify(
+        testCase.input
+      )}), then it returns ${JSON.stringify(testCase.expect)}`
+    );
+  });
+
+  t.end();
+});
