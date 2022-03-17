4e27f69ee3507ccde6213677fdd0b375e1612825
wuxue <wuxue@sensorsdata.cn>
2022-03-17 10:31:37 +0800

    UT Case

---
+++ test/searchObjDate.test.js
@@ -0,0 +1,48 @@
+import test from 'tape';
+import searchObjDate from '../src/searchObjDate';
+
+const testCases = [
+  {
+    input: { a: new Date('2020-02-02 8:0:12') },
+    expect: { a: '2020-02-02 08:00:12.00' },
+  },
+  {
+    input: 'test',
+    expect: 'test',
+  },
+  {
+    input: {
+      a: new Date('2020-02-02 8:0:12'),
+      b: {
+        b_child: new Date('2020-02-03 8:0:12'),
+      },
+      c: 'test',
+    },
+    expect: {
+      a: '2020-02-02 08:00:12.00',
+      b: {
+        b_child: '2020-02-03 08:00:12.00',
+      },
+      c: 'test',
+    },
+  },
+];
+
+test('test searchObjDate function', (t) => {
+  // var val;
+  // searchObjDate(testCases[0].input);
+  // // console.log(val);
+  // t.deepEqual(testCases[0].input, testCases[0].expect);
+
+  testCases.forEach((testCase) => {
+    searchObjDate(testCase.input);
+    t.deepEqual(
+      testCase.input,
+      testCase.expect,
+      `call searchObjDate(${JSON.stringify(
+        testCase.input
+      )}), and then the input object turns to ${JSON.stringify(testCase.expect)}`
+    );
+  });
+  t.end();
+});
