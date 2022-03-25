96b7fba25e4b81d2c3a5cfbe2202c0fc13db6e14
wuxue <wuxue@sensorsdata.cn>
2022-03-25 15:48:25 +0800

    Update UT Case

--- test/loadScript.test.js
+++ test/loadScript.test.js
@@ -2,174 +2,172 @@ import test from 'tape';
 import sinon from 'sinon';
 import loadScript from '../src/loadScript';
 
+// mock head
+const mockHead = {
+  nodeType: 1,
+  tagName: 'head',
+  children: [],
+  appendChild: function (ele) {
+    this.children.push(ele);
+  },
+};
+// mock link
+const mockLink = {
+  nodeType: 1,
+  tagName: 'link',
+  rel: '',
+  href: '',
+  onload: function () {},
+  onreadystatechange: function () {},
+  onerror: function () {},
+};
+// mock script
+const mockScript = {
+  nodeType: 1,
+  tagName: 'script',
+  charset: '',
+  async: '',
+  src: '',
+  type: '',
+  readyState: undefined,
+  setAttribute: function () {},
+  onload: function () {},
+  onreadystatechange: function () {},
+  onerror: function () {},
+};
+
+// 模拟 document 事件
+global.document = {
+  createElement: function () {},
+  getElementsByTagName: function () {},
+};
+
 test('test loadScript function', (t) => {
-  // mock head
-  const mockHead = {
-    nodeType: 1,
-    tagName: 'head',
-    children: [],
-    appendChild: function (ele) {
-      this.children.push(ele);
-    },
-  };
-  // mock link
-  const mockLink = {
-    nodeType: 1,
-    tagName: 'link',
-    rel: '',
-    href: '',
-    onload: function () {},
-    onreadystatechange: function () {},
-    onerror: function () {},
-  };
-  // mock script
-  const mockScript = {
-    nodeType: 1,
-    tagName: 'script',
-    charset: '',
-    async: '',
-    src: '',
-    type: '',
-    readyState: undefined,
-    setAttribute: function () {},
-    onload: function () {},
-    onreadystatechange: function () {},
-    onerror: function () {},
-  };
+  test('test loadScript function when script type is js', (t) => {
+    // js para
+    var para = {
+      url: '/test.js',
+      type: 'js',
+      success: function () {
+        console.log('js script load succeed');
+      },
+      onerror: function () {
+        console.log('js script load error');
+      },
+    };
 
-  // 模拟 document 事件
-  global.document = {
-    createElement: function () {},
-    getElementsByTagName: function () {},
-  };
+    // 加载 js 脚本
+    sinon
+      .stub(global.document, 'getElementsByTagName')
+      .withArgs('head')
+      .returns([mockHead]);
+    sinon
+      .stub(global.document, 'createElement')
+      .withArgs('script')
+      .returns(mockScript);
+    sinon.stub(mockScript, 'setAttribute').callsFake(function func(v, k) {
+      mockScript[v] = k;
+    });
 
-  // js para
-  var para = {
-    url: '/test.js',
-    type: 'js',
-    success: function () {
-      console.log('js script load succeed');
-    },
-    onerror: function () {
-      console.log('js script load error');
-    },
-  };
+    loadScript(para);
+    // 是否正确设置属性
+    const cases = [
+      { k: 'async', v: 'async' },
+      { k: 'charset', v: 'UTF-8' },
+      { k: 'src', v: para.url },
+      { k: 'type', v: 'text/javascript' },
+    ];
+    cases.forEach((item) => {
+      t.equal(
+        mockScript[item.k],
+        item.v,
+        `call loadScript when script type is js, then as expected, mockScript.${item.k} is "${item.v}"`
+      );
+    });
 
-  // 加载 js 脚本
-  sinon
-    .stub(global.document, 'getElementsByTagName')
-    .withArgs('head')
-    .returns([mockHead]);
-  sinon
-    .stub(global.document, 'createElement')
-    .withArgs('script')
-    .returns(mockScript)
-    .withArgs('link')
-    .returns(mockLink);
-  sinon.stub(mockScript);
+    // test g.onerror
+    mockScript.onerror();
+    t.equals(mockScript.onerror, null, 'g.onerror performs as expected');
 
-  loadScript(para);
-  mockScript.charset = 'UTF-8';
-  // 是否正确设置属性
-  const cases = [
-    { k: 'async', v: 'async' },
-    { k: 'charset', v: 'UTF-8' },
-    { k: 'src', v: para.url },
-    { k: 'type', v: 'text/javascript' },
-  ];
-  cases.forEach((item) => {
-    t.equal(
-      mockScript[item.k],
-      item.v,
-      `call loadScript(para) when para.type is js, then as expected mockScript.${item.k} is "${item.v}"`
-    );
-  });
+    // test g.onload & g.onreadystatechange
+    // 当 !this.readyState || this.readyState = loaded || complete;
+    var stub = sinon.stub(mockScript, 'onload');
+    var stub2 = sinon.stub(mockScript, 'onreadystatechange');
+    [undefined, 'loaded', 'complete'].forEach((item) => {
+      ['onload', 'onreadystatechange'].forEach((curr) => {
+        stub.restore();
+        stub2.restore();
+        sinon.stub(mockScript, 'readyState').value(item);
+        mockScript[curr]();
+        ['onload', 'onreadystatechange'].forEach((i) => {
+          t.equal(
+            mockScript[i],
+            null,
+            `call g.${curr} when mockScript.readyState is ${JSON.stringify(
+              item
+            )}, then g.${i} turns to null`
+          );
+        });
+      });
+    });
 
-  // g.onerror
-  mockScript.onerror();
-  t.equals(mockScript.onerror, null, 'g.onerror performs as expected');
-
-  // g.onload
-  // this.readyState = loaded || complete
-  var stub = sinon.stub(mockScript, 'onload');
-  ['loaded', 'complete'].forEach((item) => {
+    // 当 !(!this.readyState || this.readyState = loaded || complete)
     stub.restore();
-    // this.readyState == 'loaded'
-    sinon.stub(mockScript, 'readyState').value(item);
-    mockScript.onload();
-    t.equal(
-      mockScript.onload,
-      null,
-      `call g.onload when mockScript.readyState is "${item}", then g.onload is null`
-    );
-    t.equal(
-      mockScript.onreadystatechange,
-      null,
-      `call g.onload when mockScript.readyState is "${item}", then g.onreadystatechange is null`
-    );
+    stub2.restore();
+    sinon.stub(mockScript, 'readyState').value('test');
+    ['onload', 'onreadystatechange'].forEach((value) => {
+      mockScript[value]();
+      ['onload', 'onreadystatechange'].forEach((v) => {
+        t.notEqual(
+          mockScript[v],
+          null,
+          `call g.${value} when mockScript.readyState !== null && this.readyState !== 'loaded' && this.readyState !== 'complete'), then g.${v} won't turn to null`
+        );
+      });
+    });
+    sinon.restore();
+    t.end();
   });
 
-  //  !this.readyState
-  stub.restore();
-  mockScript.readyState = undefined;
-  mockScript.onload();
-  t.equal(
-    mockScript.onload,
-    null,
-    'g.onload perfoms as expected when mockScript.readyState == undefined'
-  );
-  t.equal(
-    mockScript.onreadystatechange,
-    null,
-    'g.onload perfoms as expected when mockScript.readyState == undefined'
-  );
+  test('test loadScript function when script type is css', (t) => {
+    // 加载 css 脚本
+    const cssPara = {
+      url: '/test.css',
+      type: 'css',
+      success: function () {
+        console.log('css script load succeed');
+      },
+      onerror: function () {
+        console.log('css script load error');
+      },
+    };
 
-  // !!this.readyState && this.readyState !== 'loaded' && this.readyState !== 'complete'
-  stub.restore();
-  mockScript.readyState = 'test';
-  mockScript.onload();
-  t.notEqual(
-    mockScript.onload,
-    null,
-    'g.onload perfoms as expected when mockScript.readyState == "test"'
-  );
+    sinon.stub(mockLink);
+    sinon
+      .stub(global.document, 'getElementsByTagName')
+      .withArgs('head')
+      .returns([mockHead]);
+    sinon
+      .stub(global.document, 'createElement')
+      .withArgs('link')
+      .returns(mockLink);
+    loadScript(cssPara);
+    const testCases = [
+      { k: 'rel', v: 'stylesheet' },
+      { k: 'href', v: cssPara.url },
+    ];
+    testCases.forEach((testCase) => {
+      t.equal(
+        mockLink[testCase.k],
+        testCase.v,
+        `call loadScript(para) when para.type is css, then as expected, mockLink.${testCase.k} turns to "${testCase.v}"`
+      );
+    });
 
-  sinon.restore();
-
-  // 加载 css 脚本
-  const cssPara = {
-    url: '/test.css',
-    type: 'css',
-    success: function () {
-      console.log('css script load succeed');
-    },
-    onerror: function () {
-      console.log('css script load error');
-    },
-  };
-
-  sinon.stub(mockLink);
-  sinon
-    .stub(global.document, 'getElementsByTagName')
-    .withArgs('head')
-    .returns([mockHead]);
-  sinon
-    .stub(global.document, 'createElement')
-    .withArgs('link')
-    .returns(mockLink);
-  loadScript(cssPara);
-  const testCases = [
-    { k: 'rel', v: 'stylesheet' },
-    { k: 'href', v: cssPara.url },
-  ];
-  testCases.forEach((testCase) => {
-    t.equal(
-      mockLink[testCase.k],
-      testCase.v,
-      `call loadScript(para) when para.type is css, then as expected, mockLink.${testCase.k} is "${testCase.v}"`
-    );
+    sinon.restore();
+    delete global.Document;
+    t.end();
   });
 
-  sinon.restore();
   t.end();
 });
