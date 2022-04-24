ab1b93bc4045195bfadca7c87612d0b10313e599
llli0v0 <llli0vv0@gmail.com>
2022-04-24 16:44:31 +0800

    delete repeat now

--- dist/wrench-umd.js
+++ dist/wrench-umd.js
@@ -3002,20 +3002,6 @@
     }
   };
 
-  /** 获取当前时间相对于 1970-01-01 00:00:00 经过的毫秒数
-   * @category Util
-   * @function now
-   * @returns {Number} 返回当前时间相对于 1970-01-01 00:00:00 经过的毫秒数
-   * @example 
-   * now() // 1646122486530
-   */
-  function now() {
-    if (Date.now && isFunction(Date.now)) {
-      return Date.now();
-    }
-    return new Date().getTime();
-  }
-
   /** 删除传入字符串开头的 'javascript'
    * 
    * @param {String} str 传入字符串
